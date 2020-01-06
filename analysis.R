817168a54eb65c5e38bcd75a4e8e31b897d1f070
Quentin Read <quentin.read@gmail.com>
2020-01-06 14:04:23 -0500

    Commit initial version of all r code, before testing

---
+++ analysis.R
@@ -0,0 +1,228 @@
+#################################################################################################################################################################
+# Read, Q. D., et al. 2019. Assessing the environmental impacts of halving food loss and waste along the food supply chain. Science of the Total Environment.   #
+#                                                                                                                                                               #
+# Code to reproduce all analyses in manuscript, beginning with cleaned data                                                                                     #
+# Last modified by QDR on 06 January 2020                                                                                                                       #
+# Contact: qread@sesync.org                                                                                                                                     #
+#################################################################################################################################################################
+
+#########################################  
+# Source scripts needed to run analysis #
+#########################################
+
+library(tidyverse)
+library(reticulate)
+library(parallel)
+
+# To reproduce this analysis, it is necessary to use a modified version of the USEEIO model (included with this repo)
+# Source R script used to build USEEIO model for each scenario.
+source('USEEIO/R/Model Build Scripts/USEEIO2012_buildfunction.R')
+model_build_path <- file.path('USEEIO/useeiopy/Model Builds')
+
+# Source Python script to call the built EEIO models can be called from R
+source_python('code/eeio_lcia.py')
+
+# Source other R scripts needed to carry out analysis.
+source('code/R_functions.R')
+
+########################
+# Load raw data into R #
+########################
+
+# Food loss and waste rates by food type and food supply chain stage
+flw_rates <- read.csv('flw_rates.csv', stringsAsFactors = FALSE)
+
+# BEA industries from input-output table with the proportion of each industry assigned to the food supply chain,
+# and within each industry the proportion assigned to each food commodity group
+industry_proportions <- read.csv('industry_proportions.csv', stringsAsFactors = FALSE) %>%
+  filter(food_system %in% c('partial', 'y')) %>%
+  arrange(stage) %>%
+  mutate(stage_code = case_when(
+    stage %in% 'agriculture' ~ 'L1',
+    stage %in% 'processing' ~ 'L2',
+    stage %in% c('retail', 'wholesale') ~ 'L3',
+    stage %in% 'foodservice' ~ 'L4a',
+    stage %in% 'institutional' ~ 'L4b'
+  ))
+
+# Make and use tables used to build the EEIO model, with 2012 values mapped to the 2007 BEA schema
+M <- read.csv('make2012.csv', row.names = 1, check.names = FALSE)
+U <- read.csv('use2012.csv', row.names = 1, check.names = FALSE)
+
+# Table to correct the formatting of the BEA codes
+bea_code_formats <- read.csv('industry_codes.csv', stringsAsFactors = FALSE)
+
+############################################################################
+# Create vectors of industry names and industry codes in different formats #
+############################################################################
+
+sector_stage_codes <- naics_foodsystem$stage_code
+sector_long_names <- all_codes$sector_desc_drc[match(naics_foodsystem$BEA_389_code, all_codes$sector_code_uppercase)]
+sector_short_names <- naics_foodsystem$BEA_389_code
+final_demand_sector_codes <- sector_stage_codes
+final_demand_sector_codes[final_demand_sector_codes %in% c('L1', 'L2', 'L3')] <- 'L5'
+
+#################################################################################################### 
+# Calculate 0%, 50% and 100% FLW reduction environmental impact values for each supply chain stage #
+####################################################################################################
+
+# Get baseline waste rate for each industry using the stage it belongs to and weighted average of the food categories in that industry
+# Original version has equal proportions of all relevant food categories in each industry
+# Later will be replaced with a correct weighted average
+flw_rates <- flw_rates %>%
+  mutate(L1 = loss_ag_production,
+         L2 = 1 - (1 - loss_handling_storage) * (1 - loss_processing_packaging),
+         L3 = loss_distribution,
+         L4a = loss_consumption,
+         L4b = loss_consumption)
+
+waste_rate_bysector <- t(flw_rates[, industry_proportions$stage_code])
+food_category_weights <- industry_proportions %>% select(cereals:beverages)
+baseline_waste_rate <- rowSums(waste_rate_bysector * food_category_weights, na.rm = TRUE) / rowSums(food_category_weights)
+
+# Function to get change in demand given original waste rate W0, reduction rate r, and proportion of the industry's output that is food p
+demand_change_fn <- function(W0, r, p) p * ((1 - W0) / (1 - (1 - r) * W0) - 1) + 1
+
+# Use 5 levels of reduction for each stage. 0%, 50%, 100%
+# 3 levels of reduction in 6 stages = 3^6 scenarios = 729
+
+rate_levels <- c(0, 0.5, 1)
+reduction_rate_grid <- expand.grid(L1 = rate_levels, L2 = rate_levels, L3 = rate_levels, L4a = rate_levels, L4b = rate_levels, L5 = rate_levels)
+
+# Create list from grid
+reduction_rate_grid_list <- setNames(split(reduction_rate_grid, seq(nrow(reduction_rate_grid))), rownames(reduction_rate_grid))
+
+# Reducing final demand in stages 1-3 modifies inputs to industry columns in classes 1-3.
+# Reducing final demand in stages 4a and 4b modifies inputs to industry columns and reduction to the corresponding rows of PCE final demand (4a and 4b respectively)
+# Reducing final demand in stage 5 does not modify any inputs to industry columns. It modifies PCE final demand in classes 1-3 but not 4a or 4b.
+
+# For each combination scenario, put together a vector of demand reduction factors for intermediate demand with values and names equal to the column names of the DRC matrix
+# Also put together a vector of demand reduction factors for final demand with values and names equal to the row names of the DRC matrix. This will modify the PCE column of final demand.
+
+# Run in parallel with mcmapply (if desired)
+
+eeio_result_grid <- mcmapply(get_reduction, reduction_by_stage = reduction_rate_grid_list, scenario_id = 1:length(reduction_rate_grid_list), mc.cores = 4, SIMPLIFY = FALSE)
+eeio_result_grid_df <- bind_rows(eeio_result_grid)
+
+# Put output into data frame and write to CSV.
+eeio_result_grid_df <- cbind(reduction_rate_grid[rep(1:nrow(reduction_rate_grid), each = 21), ], eeio_result_grid_df)
+
+write.csv(eeio_result_grid_df, file = 'sixstage_scenario_grid_lcia_results.csv', row.names = FALSE)
+
+###########################
+# Do uncertainty analysis #
+###########################
+
+# Function to generate a sampling draw for proportion food, if it's less than 1.
+# Accepts input of p (vector) and factor for uncertainty of beta distribution. So each is beta(fp, f(1-p))
+proportion_food_draw <- function(p, f = 100) rbeta(n = length(p), shape1 = f * p, shape2 = f * (1 - p))
+
+# Function to generate a sampling draw for the baseline waste rates.
+# Assume no relationship among the various loss rates, whether within category or within stage
+# This could be modified by replacing beta distributions with Dirichlet distributions
+# Accepts input of w, which is a matrix of the loss rates, and f, factor for uncertainty of beta distribution
+baseline_rate_table_draw <- function(w, f = 100) {
+  res <- apply(w, 2, function(p) rbeta(n = length(p), shape1 = f * p, shape2 = f * (1 - p)))
+  res[!is.finite(res)] <- NA
+  res
+}
+
+n_draws <- 100 # Number of rounds of uncertainty analysis
+uncertainty_factor <- 100 # The higher this number, the LOWER the uncertainty.
+set.seed(333)
+
+# Only use 0% reduction (baseline) and 50% reduction for each stage
+# Then do 0 and 100% separately
+
+reduction_rate_grid <- rbind(expand.grid(replicate(6, c(0, 0.5), simplify = FALSE)),
+							 expand.grid(replicate(6, c(0, 1), simplify = FALSE))[-1,])
+names(reduction_rate_grid) <- c('L1', 'L2', 'L3', 'L4a', 'L4b', 'L5')
+
+# Create list from grid
+reduction_rate_grid_list <- split(reduction_rate_grid, seq(nrow(reduction_rate_grid))) %>%
+  map(unlist)
+
+# Take random draws around mean for FAO waste rate
+faopct_list <- replicate(n_draws, baseline_rate_table_draw(w = faopct %>% select(loss_ag_production:loss_consumption), f = uncertainty_factor), simplify = FALSE)
+
+# Get L1 - L4b values from each draw
+faopct_list <- map(faopct_list, ~ .x %>%
+                     as.data.frame %>%
+                     mutate(L1 = loss_ag_production,
+                            L2 = 1 - (1 - loss_handling_storage) * (1 - loss_processing_packaging),
+                            L3 = loss_distribution,
+                            L4a = loss_consumption,
+                            L4b = loss_consumption))
+
+# Take random draws around mean for proportion food in each sector
+proportion_food_list <- replicate(n_draws, proportion_food_draw(p = naics_foodsystem$proportion_food, f = uncertainty_factor), simplify = FALSE)
+
+waste_rate_bysector_list <- map(faopct_list, ~ t(.x[, naics_foodsystem$stage_code]))
+fao_category_weights <- naics_foodsystem %>% select(cereals:beverages)
+baseline_waste_rate_list <- map(waste_rate_bysector_list, ~ rowSums(.x * fao_category_weights, na.rm = TRUE) / rowSums(fao_category_weights))
+
+# Cross the lists of reduction rates with the different parameters
+# Total number of models to run: number of draws * number of reduction rate combinations
+baseline_waste_rate_combos <- cross2(reduction_rate_grid_list, baseline_waste_rate_list)
+proportion_food_combos <- cross2(reduction_rate_grid_list, proportion_food_list)
+																						 
+sensitivity_arguments <- list(reduction_rate = map(baseline_waste_rate_combos, 1),
+                              baseline_waste_rate = map(baseline_waste_rate_combos, 2),
+                              proportion_food = map(proportion_food_combos, 2),
+                              scenario_id = as.list(paste0('scenario', 1:length(baseline_waste_rate_combos))))
+
+# Run uncertainty analysis in parallel with mcmapply
+eeio_result_grid_sensitivity <- with(sensitivity_arguments, mcmapply(get_reduction_from_list, reduction_rate, baseline_waste_rate, proportion_food, scenario_id, mc.cores = 4, SIMPLIFY = FALSE))
+
+# Match output with arguments to get confidence intervals
+grid_sensitivity_df <- map2_dfr(sensitivity_arguments$reduction_rate, eeio_result_grid_sensitivity, ~ data.frame(t(.x), .y))
+
+grid_sensitivity_CIs <- grid_sensitivity_df %>%
+  group_by(L1, L2, L3, L4a, L4b, L5, impact_category) %>%
+  do(quantile(.$value, probs = c(0.025, 0.25, 0.5, 0.75, 0.975)) %>% t %>% as.data.frame %>% setNames(c('q025', 'q25', 'q50', 'q75', 'q975')))
+
+# Write confidence intervals to CSV
+write.csv(grid_sensitivity_CIs, 'sensitivity_grid_CIs.csv', row.names = FALSE)
+
+################################################################################
+# Calculate environmental impact reductions for food-specific waste reductions #
+################################################################################
+
+# Get waste rates by sector x commodity
+waste_rate_bycommodityxsector <- waste_rate_bysector * fao_category_weights
+
+# Specify environmental impact categories and food commodity groups to minimize
+
+# Environmental goods to minimize
+categories <- c("impact potential/gcc/kg co2 eq", "resource use/land/m2*yr", "resource use/watr/m3", "resource use/enrg/mj", "impact potential/eutr/kg n eq")
+
+# Commodities to target
+commodities <- names(fao_category_weights)
+
+# All combinations of category x commodity
+scenarios <- expand.grid(category = categories, commodity = commodities, stringsAsFactors = FALSE)
+
+# Run in parallel across all combinations
+results_by_commodity <- mcmapply(find_best_pathway, category = scenarios$category, commodity = scenarios$commodity, mc.cores = 4, SIMPLIFY = FALSE)
+results_by_commodity <- tibble(category = scenarios$category, 
+                               commodity = scenarios$commodity, 
+                               best_pathway = map(results_by_commodity, 'best_pathway'), 
+                               impacts = map(results_by_commodity, 'best_pathway_impacts'))
+
+# Get baseline values to normalize results
+baseline_result <- eeio_result_grid_df %>%
+  filter(scenario == 1)
+  
+# Convert list columns of tibble into regular columns
+# Keep only the environmental good being minimized
+results_by_commodity_df <- results_by_commodity %>% 
+  pmap_dfr(function(category, commodity, best_pathway, impacts) data.frame(category = category, 
+                                                                           commodity = commodity,
+                                                                           n_stages_reduced = 1:length(best_pathway),
+                                                                           stage_reduced = best_pathway,
+                                                                           impact = map_dbl(impacts, ~ .x$value[.x$impact_category == category]))) %>%
+  left_join(baseline_result %>% select(impact_category, value), by = c('category'='impact_category')) %>% 
+  rename(baseline_impact = value) %>%
+  mutate(impact_norm = impact / baseline_impact)
+  
+write.csv(results_by_commodity_df, 'bestpathway_bycommodity.csv', row.names = FALSE)
---
+++ code/R_functions.R
@@ -0,0 +1,182 @@
+# ===================
+# get_eeio_result() =
+# ===================
+
+# Function to build the USEEIO model with modified intermediate and final demand values and return the result.
+# Takes several inputs: 
+# c_factor is the factor to multiply the intermediate column inputs (vector), 
+# r_factor is the factor to multiply the rows of the final demand column (vector),   
+# c_names is the names of the columns to multiply by c_factor (vector with same length as c_factor)
+# r_names is the names of the rows to multiply by r_factor (vector with same length as r_factor)
+# i is the name of the scenario
+# By default the only final demand column we modify is F01000: personal consumption expenditures (PCE)
+
+get_eeio_result <- function(c_factor, r_factor, c_names, r_names, i = 'no_name') {
+
+  # Step 3. Build USEEIO with specifications for modifying intermediate and final demand.
+  build_USEEIO(outputfolder = file.path(model_build_path, paste0('scenario_', i)),
+               model = paste0('scenario_', i),
+               usetablefile = file.path(fp_bea, 'use2012.csv'),
+               maketablefile = file.path(fp_bea, 'make2012.csv'),
+               code_path = fp_useeio,
+               intermediate_columns_modify = c_names,
+               intermediate_change_factor = c_factor,
+               final_rows_modify = r_names,
+               final_columns_modify = c('F01000'),
+               final_change_factor = r_factor
+  )
+  # Step 4. Extract demand vector for food system from scenario.
+  # Join this with the food system proportions and with the correct demand codes (full description names)
+  all_final_demand <- read.csv(file.path(model_build_path, paste0('scenario_', i), paste0('scenario_', i, '_FinalDemand.csv')), stringsAsFactors = FALSE) %>%
+    left_join(naics_foodsystem, by = c('BEA_389_code', 'BEA_389_def')) %>% 
+    filter(!is.na(proportion_food)) %>%
+    left_join(all_codes, by = c('BEA_389_code' = 'sector_code_uppercase'))
+  # Convert demand vector to separate list of codes and values
+  final_demand_list <- with(all_final_demand, list(codes = as.list(sector_desc_drc), values = as.list(X2012_US_Consumption * proportion_food)))
+  # Step 5. Run the model!
+  eeio_result <- eeio_lcia(paste0('scenario_', i), final_demand_list$values, final_demand_list$codes) 
+  eeio_result <- data.frame(scenario = i, impact_category = rownames(eeio_result), value = eeio_result$Total, stringsAsFactors = FALSE)
+  # Step 6. Delete the intermediate files (entire model build folder) 
+  unlink(file.path(model_build_path, paste0('scenario_', i)), recursive = TRUE)
+  
+  return(eeio_result)
+}
+
+# =================
+# get_reduction() =
+# =================
+
+# Wrapper function so that get_eeio_result() is compatible with parallel::mcmapply()
+
+get_reduction <- function(reduction_by_stage, scenario_id) {
+  intermediate_demand_change_factors <- as.numeric(demand_change_fn(baseline_waste_rate, reduction_by_stage[sector_stage_codes], naics_foodsystem$proportion_food))
+  final_demand_change_factors <- as.numeric(demand_change_fn(baseline_waste_rate, reduction_by_stage[final_demand_sector_codes], naics_foodsystem$proportion_food))
+  get_eeio_result(c_factor = intermediate_demand_change_factors,
+                  c_names = sector_long_names,
+                  r_factor = final_demand_change_factors,
+                  r_names = sector_short_names,
+                  i = scenario_id)
+}
+
+# ===============================
+# get_eeio_result_uncertainty() =
+# ===============================
+
+# Function to get results from EEIO with different values from the uncertainty analysis.
+
+get_eeio_result_uncertainty <- function(c_factor, r_factor, c_names, r_names, i = 'no_name', crosswalk = naics_foodsystem) {
+  
+  # Build USEEIO with specifications for modifying intermediate and final demand.
+  build_USEEIO(outputfolder = file.path(model_build_path, paste0('scenario_', i)),
+               model = paste0('scenario_', i),
+               usetablefile = file.path(fp_bea, 'use2012.csv'),
+               maketablefile = file.path(fp_bea, 'make2012.csv'),
+               code_path = fp_useeio,
+               intermediate_columns_modify = c_names,
+               intermediate_change_factor = c_factor,
+               final_rows_modify = r_names,
+               final_columns_modify = c('F01000'),
+               final_change_factor = r_factor
+  )
+  # Extract demand vector for food system from scenario.
+  # Join this with the food system proportions and with the correct demand codes (full description names)
+  all_final_demand <- read.csv(file.path(model_build_path, paste0('scenario_', i), paste0('scenario_', i, '_FinalDemand.csv')), stringsAsFactors = FALSE) %>%
+    left_join(crosswalk, by = c('BEA_389_code', 'BEA_389_def')) %>% 
+    filter(!is.na(proportion_food)) %>%
+    left_join(all_codes, by = c('BEA_389_code' = 'sector_code_uppercase'))
+  # Convert demand vector to separate list of codes and values
+  final_demand_list <- with(all_final_demand, list(codes = as.list(sector_desc_drc), values = as.list(X2012_US_Consumption * proportion_food)))
+  # Step 5. Run the model!
+  eeio_result <- eeio_lcia(paste0('scenario_', i), final_demand_list$values, final_demand_list$codes) 
+  eeio_result <- data.frame(scenario = i, impact_category = rownames(eeio_result), value = eeio_result$Total, stringsAsFactors = FALSE)
+  # Step 6. Delete the intermediate files (entire model build folder) 
+  unlink(file.path(model_build_path, paste0('scenario_', i)), recursive = TRUE)
+  
+  return(eeio_result)
+}
+
+# =============================
+# get_reduction_uncertainty() =
+# =============================
+
+# Wrapper function so that get_eeio_result_uncertainty() is compatible with parallel::mcmapply()
+
+get_reduction_from_list <- function(reduction_rate, baseline_waste_rate, proportion_food, scenario_id) {
+  intermediate_demand_change_factors <- as.numeric(demand_change_fn(baseline_waste_rate, reduction_rate[sector_stage_codes], proportion_food))
+  final_demand_change_factors <- as.numeric(demand_change_fn(baseline_waste_rate, reduction_rate[final_demand_sector_codes], proportion_food))
+  get_eeio_result_uncertainty(c_factor = intermediate_demand_change_factors,
+                              c_names = sector_long_names,
+                              r_factor = final_demand_change_factors,
+                              r_names = sector_short_names,
+                              i = scenario_id,
+                              crosswalk = naics_foodsystem %>% mutate(proportion_food = !!proportion_food))
+}
+
+# =====================
+# evaluate_scenario() =
+# =====================
+
+# Wrapper function to evaluate USEEIO for a single scenario, specifying the waste rate for the food commodity group we want.
+evaluate_scenario <- function(reduction_by_stage, baseline_waste_rate, scenario_id) {
+  intermediate_demand_change_factors <- as.numeric(demand_change_fn(baseline_waste_rate, reduction_by_stage[sector_stage_codes], naics_foodsystem$proportion_food))
+  final_demand_change_factors <- as.numeric(demand_change_fn(baseline_waste_rate, reduction_by_stage[final_demand_sector_codes], naics_foodsystem$proportion_food))
+  intermediate_demand_change_factors[is.na(intermediate_demand_change_factors)] <- 1
+  final_demand_change_factors[is.na(final_demand_change_factors)] <- 1
+  get_eeio_result(c_factor = intermediate_demand_change_factors,
+                  c_names = sector_long_names,
+                  r_factor = final_demand_change_factors,
+                  r_names = sector_short_names,
+                  i = scenario_id)
+}
+
+# =====================
+# find_best_pathway() =
+# =====================
+
+# Function to find the sequence of 50% reductions of waste in a chosen food commodity group that minimizes the chosen environmental category
+
+find_best_pathway <- function(category, commodity) {
+  # Find the waste rates for the commodity being evaluated
+  commodity_waste_rates <- waste_rate_bycommodityxsector[, commodity]
+  
+  # Find the stages across which we will optimize (many will have 6 but some that aren't in agriculture stage will only have 5)
+  stages <- c('L2','L3','L4a','L4b','L5')
+  if (!is.na(commodity_waste_rates[1])) stages <- c('L1', stages)
+  
+  # Initialize best pathway and other structures to hold data
+  best_pathway <- c()
+  best_pathway_impacts <- list()
+  reductions <- setNames(rep(0, 6), c('L1', 'L2', 'L3', 'L4a', 'L4b', 'L5'))
+  
+  # Each time the iteration runs, we select one stage from the remaining not yet chosen stages and add it to the already chosen stages
+  # Then run the scenario and see which stage minimizes the environmental impact in the chosen good
+  # This is still done even when there is only one stage left since we want to know the impacts when the one final stage is reduced
+  for (i in 1:length(stages)) {
+    # Print a progress message
+    message('Evaluating stage ', i, ' for ', commodity, ' minimizing ', category)
+    
+    # Define each of the remaining scenarios
+    candidate_scenarios <- sapply(stages, function(x) {
+      to_reduce <- c(best_pathway, x)
+      reductions[to_reduce] <- 0.5
+      return(reductions)
+    }, simplify = FALSE)
+    
+    # Evaluate each scenario for the given commodity only (just serially, not in parallel, since there are at most 6)
+    scenario_results <- map(candidate_scenarios, evaluate_scenario, baseline_waste_rate = commodity_waste_rates, scenario_id = paste(commodity, gsub('[^a-z]', '', category), i, sep = '_'))
+    
+    # Identify which scenario minimizes the environmental good in question, and append it to best_pathway
+    impact_values <- map_dbl(scenario_results, ~ .x$value[.x$impact_category == category])
+    best_stage <- which.min(impact_values)
+    best_pathway <- c(best_pathway, stages[best_stage])
+    
+    # Remove it from the remaining candidate stages
+    stages <- stages[!stages %in% best_pathway]
+    
+    # Write the best scenario results to the list of outputs
+    best_pathway_impacts[[i]] <- scenario_results[[best_stage]]
+  }
+  
+  # Return the results
+  return(list(best_pathway = best_pathway, best_pathway_impacts = best_pathway_impacts))
+}
